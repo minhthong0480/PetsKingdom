e13ca166b8ba197379344ea3fdc7f0f12210e53d
Minh Thong <s3852882@rmit.edu.vn>
2022-08-15 04:24:10 +0700

    create logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,42 @@
 import { Link } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import { useSelector,useDispatch } from "react-redux";
+import { Fragment } from "react";
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+  const { auth } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  //functin for logout
+  const logout = () => {
+    dispatch({
+      type: "LOGOUT",
+      payload: null
+    })   
+    localStorage.removeItem('auth')  
+    navigate('/login')
+  }
   return (
     <div className="nav bg-light d-flex justify-content-between">
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/login">Login</Link>
-        <Link className="nav-link" to="/register">Register</Link>
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+
+      {auth !== null && <a className="nav-link pointer" onClick={logout}>Logout</a>}
+
+      {auth === null && (
+        <Fragment>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </Fragment>
+      )}
     </div>
-  )
+  );
 };
 
 export default Navbar;
