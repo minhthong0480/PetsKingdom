e358508842bb6052bd930032ffc432a1404826d3
Minh Thong <s3852882@rmit.edu.vn>
2022-08-29 13:01:45 +0700

    add booking form

--- client/src/components/forms/UserBookingForm.js
+++ client/src/components/forms/UserBookingForm.js
@@ -1,1 +1,58 @@
+import { Fragment, React, useState } from "react";
+import { DatePicker, Select } from "antd";
+import "antd/dist/antd.css";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
 
+const UserBookingForm = () => {
+  const [pets, setPets] = useState([]);
+  const [note, setNote] = useState([]);
+  const [date, setDate] = useState([]);
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { token } = auth;
+  const { Option } = Select;
+
+  const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current && current < moment().subtract(1, "days");
+  };
+
+  const { RangePicker } = DatePicker;
+  return (
+    <Fragment>
+      <form action="">
+        <div className="form-group">
+          <Select
+            className="w-100 m-2"
+            size="large"
+            placeholder="Choose Pet from your Collection"
+          >
+            {pets.map((pet) => (
+              <Option value={pet._id}>{pet.petname}</Option>
+            ))}
+          </Select>
+          <textarea
+            type="content"
+            name="note"
+            // onChange={handleChange}
+            placeholder="Leave a note for Staff"
+            className="form-control m-2"
+            values={note}
+          />
+          {/* <div className="ml-4">
+            <label> How long you want to book</label>
+          </div> */}
+          <RangePicker
+            className="form-control m-2"
+            disabledDate={disabledDate}
+          />
+
+          <br />
+          <button className="btn btn-outline-primary m-2">Book</button>
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
+export default UserBookingForm;
--- client/src/user/Userbooking.js
+++ client/src/user/Userbooking.js
@@ -1,11 +1,24 @@
+import moment from "moment";
 import React, { Fragment, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { allPets, userPets } from "../action/pet";
+import { userPets } from "../action/pet";
+import { DatePicker, Select } from "antd";
+import UserBookingForm from "../components/forms/UserBookingForm";
+import { useNavigate } from "react-router-dom";
 
 function Userbooking() {
   const [pets, setPets] = useState([]);
   const { auth } = useSelector((state) => ({ ...state }));
   const { token } = auth;
+  const { Option } = Select;
+  const navigate = useNavigate();
+
+  const { RangePicker } = DatePicker;
+
+  const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current && current < moment().subtract(1, "days");
+  };
 
   useEffect(() => {
     loadAllPets();
@@ -18,19 +31,11 @@ function Userbooking() {
     setPets(res.data);
   };
 
-  const UserBookingForm = () => {
-    return (
-      <select class="form-select" aria-label="Default select example">
-        {pets.map((pet) => (
-          <option value={pet._id}>{pet.petname}</option>
-        ))}
-      </select>
-    );
-  };
-
   return (
     <Fragment>
-      <div>User Booking</div>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h2>Add Booking</h2>
+      </div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-10">
